c95dd01fbb90228f29a6232c9320096ca9d86825
Avin Divakara <67309607+AVDiv@users.noreply.github.com>
2025-01-07 16:53:57 +0530

    Modified ionic configs & Added some android build configs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'storion.app',
-  appName: 'storion-app',
-  webDir: 'dist'
+  appId: "storion.app",
+  appName: "storion-app",
+  webDir: "dist",
 };
 
 export default config;
