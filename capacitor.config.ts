6b176fff7a65c84e6f24ea2af71cc5c7754c798d
Avin Divakara <67309607+AVDiv@users.noreply.github.com>
2025-01-08 06:52:08 +0530

    Enable cleartext traffic and allow mixed content for Android testing

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,9 @@ const config: CapacitorConfig = {
   appId: "storion.app",
   appName: "storion-app",
   webDir: "dist",
+  android: {
+    allowMixedContent: true, // For Testing purposes only
+  },
 };
 
 export default config;
